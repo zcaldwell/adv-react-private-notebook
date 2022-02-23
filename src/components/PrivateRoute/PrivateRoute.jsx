a066d936081b8610199c1a3d719177407b10e752
zcaldwell <mintyst.john@gmail.com>
2022-02-23 12:38:00 -0800

    privateroute written

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -4,8 +4,23 @@ import { useUser } from '../../context/UserContext';
 // A wrapper for <Route> that redirects to the login
 // screen if you're not yet authenticated.
 export default function PrivateRoute({ children, ...rest }) {
-  // TODO: Use the user in context to determine whether to redirect to /login
-  // const { user } = useUser();
+ const { user } = useUser();
 
-  return <Route {...rest} render={({ location }) => children} />;
+  return (
+  <Route 
+  {...rest} 
+    render={({ location }) => 
+    user ? ( 
+      children 
+    ) : ( 
+      <Redirect 
+        to={{ 
+          pathname: '/login', 
+          state: { from: location }, 
+          }} 
+        />
+      )
+    }
+  /> 
+  );
 }
